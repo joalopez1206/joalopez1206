811ee1353342f0c8732df7b6b043f348825d6acc
joalopez1206 <75399398+joalopez1206@users.noreply.github.com>
2022-03-24 12:47:32 -0300

    Update Tarea1.c
    añadido otra version pa ver dps

--- Tarea1.c
+++ Tarea1.c
@@ -20,6 +20,22 @@ uint borrar_bits(uint x, uint pat, int len) {
     return x;
 }
 
+
+uint borrar_bits2(uint x, uint pat, int len) {
+    int m1 = ~(-1<<len); // creamos la mascara para ver dos numeros
+
+    for(int i = 0; i<(sizeof(x)<<3)-len+1; i++){ // hacemos el ciclo
+
+        int m2 = m1 << i;
+        
+        if((x & m2) == (pat << i) ){ //si se cumple que es igual al patron
+
+            x &= ~m2; // lo hacemos 0 ya que m2 es de la forma 0000..1111.000
+        }
+    }
+    return x;
+}
+
 int main(){
     //printf("si niegas este numero ~ %d va a ser: %d \n", 0, ~0);
     printf("borrar_bits(0x%x,0x1,1) = 0x%x", 0x89,
